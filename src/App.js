0648a1b3c24a89766425e207ee9a9881fd49be22
Calente Cardwell <nxmxgoldxx@live.com>
2022-02-17 10:45:01 -0800

    new+event handler,toggle,variable onClick 'light'

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 
 class App extends Component {
   state = { on: true };
+  toggle = () => {
+    const is0n = this.state.on;
+    this.setState({ on: !is0n }); 
+  };
   render() {
     let wallClass = "wall off";
     if (this.state.on) {
       wallClass = "wall on";
+/*JavaScript expression to toggle the state of a light switch*/
     }
-    return (
+    return (                    
       <div className={wallClass}>
         <div className="switch-plate">
           <div className="screw" />
-          <div className="switch">
+          <div className="switch" onClick={this.toggle}>
             <div className="switch-handle" />
           </div>
           <div className="screw" />
